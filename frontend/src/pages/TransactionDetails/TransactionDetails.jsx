f81930a66a186a6c442bbce0912e98974354e469
Forcica <47173413+Forcica@users.noreply.github.com>
2024-10-10 14:11:08 +0200

    [FIX] Updated the swagger file and

--- frontend/src/pages/TransactionDetails/TransactionDetails.jsx
+++ frontend/src/pages/TransactionDetails/TransactionDetails.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 const TransactionDetails = () => {
   const transactions = useSelector((state) => state.profile.transactions);
 
   return (
-    <div>
+   <div>
       <h2>Détails des opérations</h2>
       <ul>
-        {transactions.map((transaction) => (
-          <li key={transaction.id}>
+      {transactions.map((transaction) => (
+         <li key={transaction.id}>
             <p>Date : {transaction.date}</p>
             <p>Montant : {transaction.amount}</p>
             <p>Description : {transaction.description}</p>
-          </li>
-        ))}
+         </li>
+      ))}
       </ul>
-    </div>
+   </div>
   );
 };
 
